15df806bcdc6f1bfb850f6b4a26f0c87553698f5
jl <jonathan.arthur.lock@gmail.com>
2018-09-27 19:58:01 +0200

    v1

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, jsonify
+import json
+import time
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def main():
+    astro_data = None
+    now_data = None
+    errors = None
+    apikey = "AIzaSyCPkyyQ6tr1Z7U59ubaJBJicWJ7-12fTbQ"
+
+    # ISS NOW
+    iss_now_url = "http://api.open-notify.org/iss-now.json"
+    try:
+        now_r = requests.get(iss_now_url)
+    except requests.ConnectionError:
+        errors = "ConnectionError"
+    now_data = json.loads(now_r.text)
+    lati = now_data['iss_position']['latitude']
+    lon = now_data['iss_position']['longitude']
+
+    # Timestamp
+    raw_timestamp = now_data['timestamp']
+    timestamp = time.ctime(raw_timestamp)
+
+
+    # Astronaut data
+    astros_url = 'http://api.open-notify.org/astros.json'
+    try:
+        r = requests.get(astros_url)
+    except requests.ConnectionError:
+        errors = "ConnectionError for Astros"
+    astro_data = json.loads(r.text)
+    astros = astro_data['people']
+    iss_astros = [x for x in astros if x['craft'] == "ISS"]
+    other_astros = [x for x in astros if x['craft'] != "ISS"]
+
+
+    return render_template('index.html', lati=lati, lon=lon, now_data=now_data, apikey=apikey, errors=errors, iss_astros=iss_astros, other_astros=other_astros, timestamp=timestamp)
+
+if __name__ == '__main__':
+    app.run()
